0158fd17ecca03552fb056ec4628de28fe352700
Caroline Pena <carolinepena@Carolines-Air.cable.rcn.com>
2023-10-17 14:19:55 -0400

    Added example values for createOrderDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -18,11 +18,13 @@ export class CreateOrderDto {
     // checking if string and not empty - might add more validation such as checking for special characters
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({example: 'John Smith'})
     readonly customer_name: string;
 
     // checking if string and not empty - might add more validation such as checking if shipping address is a valid address (using something like Google's geocoding web service)
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({example: '123 Main Street New York, NY 12345'})
     readonly shipping_add: string;
 
     // checking if type if Purchase and not null/undefined
